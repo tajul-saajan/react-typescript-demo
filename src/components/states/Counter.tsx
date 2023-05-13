ceef8deb296551ec3104f2c6363350d647e067b0
Tajul Islam <tajul.islam@sigmasolutions.com.bd>
2023-05-13 19:39:44 +0600

    added strict typing

--- src/components/states/Counter.tsx
+++ src/components/states/Counter.tsx
@@ -1,21 +1,30 @@
+import { type } from "os";
 import { useReducer } from "react";
 type State = {
   count: number;
 };
 
-type Action = {
+type UpdateAction = {
   type: "increment" | "decrement";
   payload: number;
 };
 
+type ResetAction = {
+  type: "reset";
+};
+
+type CounterAction = UpdateAction | ResetAction;
+
 const initialState: State = { count: 0 };
 
-function reducer(state: State, action: Action): State {
+function reducer(state: State, action: CounterAction): State {
   switch (action.type) {
     case "increment":
       return { count: state.count + action.payload };
     case "decrement":
       return { count: state.count - action.payload };
+    case "reset":
+      return initialState;
     default:
       return state;
   }
@@ -32,6 +41,7 @@ export default function Counter() {
       <button onClick={() => dispatch({ type: "decrement", payload: 1 })}>
         -
       </button>
+      <button onClick={() => dispatch({ type: "reset" })}>reset</button>
     </>
   );
 }
